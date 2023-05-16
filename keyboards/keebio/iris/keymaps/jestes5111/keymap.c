6788d593ec9670dbe9063887c504699d5aedb123
Jesse Estes <jestes5111@gmail.com>
2023-05-16 15:57:07 -0400

    Final changes for today

--- keyboards/keebio/iris/keymaps/jestes5111/keymap.c
+++ keyboards/keebio/iris/keymaps/jestes5111/keymap.c
@@ -147,15 +147,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NUM] = LAYOUT(
     // ┌──────────┬──────────┬──────────┬──────────┬──────────┬──────────┐                              ┌──────────┬──────────┬──────────┬──────────┬──────────┬──────────┐
-        KC_ESC,    KC_SLSH,   KC_CIRC,   KC_PERC,   KC_DLR,    TO(_NUM),                                TO(_QWERTY),KC_NO,     KC_NO,     KC_NO,     KC_NO,     TO(_GAME),
+        KC_NO,     KC_SLSH,   KC_CIRC,   KC_PERC,   KC_DLR,    TO(_NUM),                                TO(_QWERTY),KC_NO,     KC_NO,     KC_NO,     KC_NO,     TO(_GAME),
     // ├──────────┼──────────┼──────────┼──────────┼──────────┼──────────┤                              ├──────────┼──────────┼──────────┼──────────┼──────────┼──────────┤
-        KC_CALC,   KC_ASTR,   KC_7,      KC_8,      KC_9,      KC_EXLM,                                  KC_NO,     KC_NO,     KC_LBRC,   KC_RBRC,   KC_NO,     QK_BOOT,
+        KC_NO,     KC_ASTR,   KC_7,      KC_8,      KC_9,      KC_EXLM,                                  KC_NO,     KC_NO,     KC_LBRC,   KC_RBRC,   KC_NO,     QK_BOOT,
     // ├──────────┼──────────┼──────────┼──────────┼──────────┼──────────┤                              ├──────────┼──────────┼──────────┼──────────┼──────────┼──────────┤
-        KC_F2,     KC_MINS,   KC_4,      KC_5,      KC_6,      KC_0,                                     KC_NO,     KC_NO,     KC_LPRN,   KC_RPRN,   KC_NO,     QK_MAKE,
+        KC_NO,     KC_MINS,   KC_4,      KC_5,      KC_6,      KC_0,                                     KC_NO,     KC_F2,     KC_LPRN,   KC_RPRN,   KC_NO,     QK_MAKE,
     // ├──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┐        ┌──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┤
-        KC_NUM,    KC_PLUS,   KC_1,      KC_2,      KC_3,      KC_DOT,    KC_NO,              KC_NO,     KC_NO,     KC_NO,     KC_LABK,   KC_RABK,   KC_NO,     KC_NO,
+        KC_NO,     KC_PLUS,   KC_1,      KC_2,      KC_3,      KC_DOT,    KC_EQL,             KC_NO,     KC_NO,     KC_NO,     KC_LABK,   KC_RABK,   KC_NO,     KC_NO,
     // └──────────┴──────────┴──────────┴────┬─────┴────┬─────┴────┬─────┴────┬─────┘        └────┬─────┴────┬─────┴────┬─────┴────┬─────┴──────────┴──────────┴──────────┘
-                                              KC_TAB,    KC_EQL,    KC_ENT,                        KC_TRNS,   KC_BSPC,   KC_NO
+                                              KC_TAB,    KC_ESC,    KC_ENT,                        KC_TRNS,   KC_BSPC,   KC_NO
     //                                       └──────────┴──────────┴──────────┘                   └──────────┴──────────┴──────────┘
     ),
 
