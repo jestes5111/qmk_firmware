bfaceb85735e4843bf3287e0acc9e503c408c892
Jesse Estes <jestes5111@gmail.com>
2024-01-22 09:28:02 -0500

    Fixed issue with overwriting `conflict.h`

---
+++ keyboards/keebio/iris/keymaps/jestes5111/config.h
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2023 Jesse Estes (@jestes5111)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define EE_HANDS
+#define RGBLIGHT_SPLIT
+#define RGBLIGHT_SLEEP
+#define ENABLE_COMPILE_KEYCODE
+
+#define UNICODE_SELECTED_MODES UNICODE_MODE_WINCOMPOSE
+
+#define HOME_ROW_MOD_TAPPING_TERM 300
+#define TAPPING_TERM 185
+#define TAPPING_TERM_PER_KEY
+#define QUICK_TAP_TERM 0
+#define PERMISSIVE_HOLD
+
+#define ONESHOT_TIMEOUT 400
+
+#define SWAP_HANDS_ENABLE
+
+// Saving space
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+#undef RGBLIGHT_ANIMATIONS
+#undef RGBLIGHT_EFFECT_BREATHING
+#undef RGBLIGHT_EFFECT_RAINBOW_MOOD
+#undef RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#undef RGBLIGHT_EFFECT_SNAKE
+#undef RGBLIGHT_EFFECT_KNIGHT
+#undef RGBLIGHT_EFFECT_CHRISTMAS
+#undef RGBLIGHT_EFFECT_STATIC_GRADIENT
+#undef RGBLIGHT_EFFECT_RGB_TEST
+#undef RGBLIGHT_EFFECT_ALTERNATING
+#undef RGBLIGHT_EFFECT_TWINKLE
